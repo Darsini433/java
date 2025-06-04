d9ae8844a6d6640cdfd4d7bc8530c25946ab54e8
Darsini433 <Kanaladarsini@gmail.com>
2025-06-04 16:13:27 +0530

    Add files via upload

---
+++ swap1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class swap1
+{
+	public static void main(String[] args) {
+	int a=5;
+	int b=8;
+	int temp=0;
+	temp=a;
+	a=b;
+	b=temp;
+	System.out.println(a+" "+b);
+	}
+}
+
