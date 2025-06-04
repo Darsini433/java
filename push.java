c2c114c344312005fe3effac92129b73e52d1b19
Darsini433 <Kanaladarsini@gmail.com>
2025-06-04 15:24:13 +0530

    Add files via upload

---
+++ push.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class push{
+     public static void main(String[] args){
+         Stack s=new Stack<>();
+		 s.push(1);
+         s.push(3);
+         System.out.println(s);
+
+     }
+ }
