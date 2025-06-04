21cff7e355c420e8dccb1271a911291c1d0623b7
Darsini433 <Kanaladarsini@gmail.com>
2025-06-04 14:16:11 +0530

    Add files via upload

---
+++ pop.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class pop{
+     public static void main(String[] args){
+         Stack s=new Stack<>();
+         s.push(1);
+         s.push(3);
+         System.out.println(s.peek());
+         System.out.println(s.pop());
+     }
+ }
