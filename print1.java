d43afd55be136a5461561749e95d9fd8e1aaf16c
Darsini433 <Kanaladarsini@gmail.com>
2025-06-04 14:13:48 +0530

    Add files via upload

---
+++ print1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class print1
+{
+	public static void main(String[] args) {
+	int []a=new int[5];
+	Scanner sc=new Scanner(System.in);
+	for(int i=0;i<a.length;i++)
+	{
+	     a[i]=sc.nextInt();
+	    System.out.println(a[i]);
+	}
+	}
+}
